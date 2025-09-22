311f4066660a9fc15375d01c2b2e50c6b246a035
Dirav-2712 <123054093+Dirav-2712@users.noreply.github.com>
2025-09-22 14:12:36 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,9 @@ export default function Header() {
   return (
     <header className="bg-white shadow-md sticky top-0 z-50">
       <div className="max-w-6xl mx-auto flex justify-between items-center p-4">
-        <div className="text-2xl font-bold text-blue-600"><Image src="/logo.jpg" alt="YogaBlog Logo" width={200} /></div>
+        <div className="text-2xl font-bold text-blue-600"> 
+    <Link href="/">  <a><Image src="/logo.jpg" alt="YogaBlog Logo" width={200} height={60} /></a></Link>
+    </div> 
         <nav className="space-x-4">
           <Link href="/"><a className="hover:text-blue-500">Home</a></Link>
           <Link href="/"><a className="hover:text-blue-500">Blog</a></Link>
